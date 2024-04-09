81a4ad73a4c1f1fb5b5dba0c562577ea9ff225ba
Victor Rodriguez <vicrodri@proton.me>
2024-04-09 11:50:21 +0200

    added tailwind config files

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  darkMode: 'selector',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
